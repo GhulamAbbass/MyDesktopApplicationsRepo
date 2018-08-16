28c40b096c5f50835b71f250a4913118052b26e8
GhulamAbbass <ranaabbas.prince@gmail.com>
2018-08-15 21:14:34 -0700

    This is my initial commit.

---
+++ PMS UI 3/PMS UI 3/CRVSample.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CrystalDecisions;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace PMS_UI_3
+{
+    /// <summary>
+    /// Interaction logic for CRVSample.xaml
+    /// </summary>
+    public partial class CRVSample : Window
+    {
+        public CRVSample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrintReport_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id",typeof(Int32));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Age", typeof(Int32));
+            dt.Columns.Add("Salary", typeof(decimal));
+
+            for (int i = 0; i < 6; i++)
+            {
+                int id = 1;
+                string name = "Rana Abbas";
+                int age = 22;
+                decimal sal = 2000;
+
+                dt.Rows.Add(id,name,age,sal);
+            }
+
+            SampleReport sr = new SampleReport();
+            sr.Database.Tables["DTSample"].SetDataSource(dt);
+
+            sampleCRV.ViewerCore.ReportSource = sr;
+        }
+    }
+}
---
+++ PMS UI 3/PMS UI 3/RelationDescription.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PMS_UI_3
+{
+    /// <summary>
+    /// Interaction logic for RelationDescription.xaml
+    /// </summary>
+    public partial class RelationDescription : Window
+    {
+        public RelationDescription()
+        {
+            InitializeComponent();
+        }
+    }
+}
